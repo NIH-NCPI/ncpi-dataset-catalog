0b2bbb15ca2a304b78186195398b691028b66b8b
jpaten <jonah@clevercanary.com>
2025-02-07 16:25:47 -0800

    feat: added analytics report generator (#3) (#4)

---
+++ analytics/constants.py
@@ -0,0 +1,19 @@
+# CHANGE THESE VALUES TO GENERATE NEW REPORTS
+# The start and end dates of the current month (yyyy-mm-dd)
+START_DATE_CURRENT = "2024-12-01"
+END_DATE_CURRENT = "2024-12-31"
+# The start and end dates of the prior months
+START_DATE_PRIOR = "2024-11-01"
+END_DATE_PRIOR = "2024-11-30"
+# The name of the folder in which to save the report
+PARENT_FOLDER_NAME = "December 2024 (demos)"
+
+# The name of the spreadsheet with the report
+SHEET_NAME = "NCPI Catalog"
+
+NCPI_CATALOG_ID = "367350417"
+SECRET_NAME = 'NCPI_ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
+GA_PROPERTY_PORTAL = "368678391" # AnVIL Portal - GA4
+ANALYTICS_START = "2023-05-01"
+
+OAUTH_PORT = 8082
