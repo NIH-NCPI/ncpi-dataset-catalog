9e06289cb052a29945c546b8e7ab5f1f8f8f358e
jpaten <jonah@clevercanary.com>
2025-02-07 16:53:16 -0800

    chore: ran analytics for jan 2025 (#3) (#4)

--- analytics/constants.py
+++ analytics/constants.py
@@ -1,12 +1,12 @@
 # CHANGE THESE VALUES TO GENERATE NEW REPORTS
 # The start and end dates of the current month (yyyy-mm-dd)
-START_DATE_CURRENT = "2024-12-01"
-END_DATE_CURRENT = "2024-12-31"
+START_DATE_CURRENT = "2025-01-01"
+END_DATE_CURRENT = "2025-01-31"
 # The start and end dates of the prior months
-START_DATE_PRIOR = "2024-11-01"
-END_DATE_PRIOR = "2024-11-30"
+START_DATE_PRIOR = "2024-12-01"
+END_DATE_PRIOR = "2024-12-31"
 # The name of the folder in which to save the report
-PARENT_FOLDER_NAME = "December 2024 (demos)"
+PARENT_FOLDER_NAME = "January 2025"
 
 # The name of the spreadsheet with the report
 SHEET_NAME = "NCPI Catalog"
