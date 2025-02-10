67b61deab8ec53b9a56dc7c5cbc8a66b92bc29b9
Jonah Paten <jonah@clevercanary.com>
2025-02-10 12:28:04 -0800

    feat: Analytics reports for December & January (#3) (#4) (#14)

---
+++ analytics/constants.py
@@ -0,0 +1,19 @@
+# CHANGE THESE VALUES TO GENERATE NEW REPORTS
+# The start and end dates of the current month (yyyy-mm-dd)
+START_DATE_CURRENT = "2025-01-01"
+END_DATE_CURRENT = "2025-01-31"
+# The start and end dates of the prior months
+START_DATE_PRIOR = "2024-12-01"
+END_DATE_PRIOR = "2024-12-31"
+# The name of the folder in which to save the report
+PARENT_FOLDER_NAME = "January 2025"
+
+# The name of the spreadsheet with the report
+SHEET_NAME = "NCPI Catalog"
+HISTORIC_UA_DATA_PATH = "./users_over_time_history.json"
+NCPI_CATALOG_ID = "367350417"
+SECRET_NAME = 'NCPI_ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
+GA_PROPERTY_PORTAL = "368678391" # NCPI Portal - GA4
+ANALYTICS_START = "2023-07-01"
+
+OAUTH_PORT = 8082
