84b0f2ba24f1f3507bd44fb82f17ca2a6cf62aeb
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-02-02 15:48:20 +1000

    chore: add transpilePackages for findable-ui (#149)

--- next.config.mjs
+++ next.config.mjs
@@ -11,5 +11,6 @@ export default withPlugins(
     output: "export",
     reactStrictMode: true,
     staticPageGenerationTimeout: 120,
+    transpilePackages: ["@databiosphere/findable-ui"],
   }
 );
