75f276380f442ed6b7238faa55c25aae528c9819
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-03-02 15:21:17 +1000

    fix: add table download to table option features (#209)

--- app/components/common/Table/options/core.ts
+++ app/components/common/Table/options/core.ts
@@ -1,10 +1,11 @@
 import { CoreOptions, getCoreRowModel, RowData } from "@tanstack/react-table";
 import { ROW_PREVIEW } from "@databiosphere/findable-ui/lib/components/Table/features/RowPreview/constants";
+import { TABLE_DOWNLOAD } from "@databiosphere/findable-ui/lib/components/Table/features/TableDownload/constants";
 
 export const CORE_OPTIONS: Pick<
   CoreOptions<RowData>,
   "_features" | "getCoreRowModel"
 > = {
-  _features: [ROW_PREVIEW],
+  _features: [ROW_PREVIEW, TABLE_DOWNLOAD],
   getCoreRowModel: getCoreRowModel(),
 };
